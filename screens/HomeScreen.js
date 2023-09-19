c31dc1c24fea38201df6220dcde51966ee480029
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2023-09-19 19:29:32 +0700

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, Image, TextInput, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+
+export default function HomeScreen() {
+
+  return (
+    <View className="flex-1 relative">
+        <Text>HomeScreen</Text>
+    </View>
+  )
+}
